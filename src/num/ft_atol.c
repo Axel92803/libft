d86d0a15839d36d12d5a16a2eefb7ccab0e40672
Alex Tanvuia <alex@hard-stuff.com>
2026-09-18 14:29:24 +0100

    fix(num): prevent overflow in ft_atol() by clamping to LONG_MAX/LONG_MIN like strtol()

--- src/num/ft_atol.c
+++ src/num/ft_atol.c
@@ -6,13 +6,15 @@
 /*   By: itanvuia <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/03 13:54:26 by itanvuia          #+#    #+#             */
-/*   Updated: 2026/03/03 13:54:29 by itanvuia         ###   ########.fr       */
+/*   Updated: 2026/09/18 14:30:00 by itanvuia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
 long	ft_atol(const char *nptr)
 {
-	int			sign;
+	int		sign;
 	long	res;
 
 	sign = 1;
@@ -26,6 +28,14 @@ long	ft_atol(const char *nptr)
 		nptr++;
 	}
 	while (*nptr >= '0' && *nptr <= '9')
+	{
+		if (res > (LONG_MAX - (*nptr - '0')) / 10)
+		{
+			if (sign == -1)
+				return (LONG_MIN);
+			return (LONG_MAX);
+		}
 		res = (res * 10) + (*nptr++ - '0');
+	}
 	return (res * sign);
 }
